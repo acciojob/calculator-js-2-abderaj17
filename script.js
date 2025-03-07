8fd983723a273da160e7ab02f6d50e9b34435d2d
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-03-07 21:07:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let display = document.getElementById("display");
+
+function appendValue(value) {
+ if(display.innerText === "0"){
+	 display.innerText = value;
+ }	else{
+	 display.innerText += value;
+ }
+}
+function clearDisplay() {
+display.innerText = "0";	
+}
+function calculateResult() {
+	try{
+		display.innerText = eval(display.innerText);
+	}catch(error){
+		display.innerText = "Error";
+	}
+	
+}
